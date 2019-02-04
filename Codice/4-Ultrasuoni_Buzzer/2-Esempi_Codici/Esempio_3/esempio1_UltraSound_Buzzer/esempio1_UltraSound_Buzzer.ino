20776c87f59f305a8a18119c6b5aed689d454db6
mattiaruberto <37310293+mattiaruberto@users.noreply.github.com>
2019-02-04 20:33:56 +0100

    3 esempio

--- Codice/4-Ultrasuoni_Buzzer/2-Esempi_Codici/Esempio_3/esempio1_UltraSound_Buzzer/esempio1_UltraSound_Buzzer.ino
+++ Codice/4-Ultrasuoni_Buzzer/2-Esempi_Codici/Esempio_3/esempio1_UltraSound_Buzzer/esempio1_UltraSound_Buzzer.ino
@@ -12,23 +12,13 @@ void setup() {
   libraryBuzzer.setPinBuzzer(0);
   libraryUltraSound.setUltraSoundPin(1, 2);
 }
-
 void loop() {
   //prendiamo la distanza dell'ultrasuoni che ci ritorna il metodo
   int distance = libraryUltraSound.getDistance();
-  
-  //controllo se la distanza è minore di 1m
-  if(distance <= 30){
-    //troviamo il tempo di suono in base alla distanza
-    int frequenza = map(distance, 0, 100, 20, 500);
-    //attiviamo il cicalino sulla porta 7 con frequenza 100
-    libraryBuzzer.setTone(frequenza);
-    //lo facciamo suonare per il tempo del risultato     
-    delay(10);
-  }else{
-    //si spegne il cicalino
-    libraryBuzzer.powerOff();
-  }
-  //pausa di 50millisecondi
-  delay(50);
+  //troviamo il tempo di suono in base alla distanza
+  int frequenza = map(distance, 0, 100, 0, 1000);
+  //attiviamo il cicalino con la frequenza risultante
+  libraryBuzzer.setTone(frequenza);
+  //una pausa di 100 ms tra un suono e l'altro    
+  delay(100);
 }
